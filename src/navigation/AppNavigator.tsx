e4e206097d454292eb1a4c5deb74c7811f55599e
Saw K Za <sawkza25505@gmail.com>
2025-11-14 21:01:53 +0630

    feat(nav): register CategorySongs screen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -4,9 +4,10 @@ import type { RootStackParamList } from './types';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
-import HomeScreen from '../screens/HomeScreen'; // We will create this screen next
+import HomeScreen from '../screens/HomeScreen';
 import MusicDetail from '../screens/MusicDetail';
 import FullSongsScreen from '../screens/FullSongsScreen';
+import CategorySongsScreen from '../screens/CategorySongsScreen'; // <-- NEW
 
 export default function AppNavigator() {
   return (
@@ -14,6 +15,7 @@ export default function AppNavigator() {
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       <Stack.Screen name="MusicDetail" component={MusicDetail} />
       <Stack.Screen name="FullSongs" component={FullSongsScreen} />
+      <Stack.Screen name = "CategorySongs"component={CategorySongsScreen} options={{ title: 'Songs' }} /> {/* <-- NEW */}
     </Stack.Navigator>
   );
 }
