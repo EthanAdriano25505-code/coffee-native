49cb5efa1e779fc924947f2a778a98792553cc60
Saw K Za <sawkza25505@gmail.com>
2025-12-03 11:35:01 +0630

    Fix navigation param types (RootStackParamList)

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -2,6 +2,9 @@ export type RootStackParamList = {
   Home: undefined;
   MusicDetail: { songId: string };
   FullSongs: undefined;
-  Player: { song?: any };
+  Player: { song?: any } | undefined;
+  CategorySongs: {
+    filter?: string;
+    title?: string;
+  } | undefined;
 };
-
